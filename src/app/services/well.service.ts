261cf6d309351a098cce3aff7011b34f385c70eb
Sadaf Patwekar <SadafP@citiustech.com>
2019-10-16 18:27:27 +0530

    changes in well service file

--- src/app/services/well.service.ts
+++ src/app/services/well.service.ts
@@ -7,40 +7,9 @@ import { wells } from '../data/wells';
 })
 export class WellService {
 
-  xwells = [
-    {
-        "type": "Danger",
-        "name": "Well One",
-        "sourceKey": "423ghbd89"
-    },
-    {
-        "type": "Danger",
-        "name": "Well Two",
-        "sourceKey": "67gj54657"
-    },
-    {
-        "type": "Info",
-        "name": "Well Three",
-        "sourceKey": "423gh66bd89"
-    },
-    {
-        "type": "Danger",
-        "name": "Well Four",
-        "sourceKey": "cvbn34546554"
-    },
-    {
-        "type": "Warning",
-        "name": "Well Five",
-        "sourceKey": "6778gj54657"
-    },
-    {
-        "type": "Info",
-        "name": "Well Six",
-        "sourceKey": "cvbn34546554"
-    }
-  ];
+  private data = wells;
 
   getWellData = () => {
-    return wells;
+    return this.data;
   };
 }
